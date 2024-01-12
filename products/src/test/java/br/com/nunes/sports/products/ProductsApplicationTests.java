6da46e09726981e048572efa1a198f8044daa6cd
Junior-Stranner <Ju-stranner@outlook.com>
2024-01-12 17:04:44 -0300

    first commit , begin to coding

---
+++ products/src/test/java/br/com/nunes/sports/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nunes.sports.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
